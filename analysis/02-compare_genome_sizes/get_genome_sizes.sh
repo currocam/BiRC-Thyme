dc2a14c230caf4d655676424c149c8096e8694b2
Francisco Campuzano Jiménez au726682@uni.au.dk <currocam@fe-open-01.gdk>
2023-04-16 14:19:15 +0200

    include all contigs

--- analysis/02-compare_genome_sizes/get_genome_sizes.sh
+++ analysis/02-compare_genome_sizes/get_genome_sizes.sh
@@ -1,4 +1,4 @@
 bioawk -c fastx \
-    '$name ~ /RagTag/ { print $name, length($seq) }' \
+    '{ print $name, length($seq) }' \
     < results/ragtag_scaffold/scaffold_10k_q30_GCA_024222315_infergaps/ragtag.scaffold.fasta \
     > analysis/02-compare_genome_sizes/ragtag_assembly_sizes.csv
