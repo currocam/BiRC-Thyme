d389e03b760293e54db97051c48b715d3cc841e5
Curro Campuzano <campuzanocurro@gmail.com>
2023-06-13 09:12:07 +0200

    writting 6/13

--- analysis/03-count_illumina_reads_assembly/mapped_vs_error_rate.R
+++ analysis/03-count_illumina_reads_assembly/mapped_vs_error_rate.R
@@ -2,16 +2,23 @@ library(tidyverse)
 source("analysis/00-miscellaneous/format.R")
 df <- read_csv("analysis/03-count_illumina_reads_assembly/samtools_stats.csv")|>
   mutate(Hyb = as.factor(Hyb))
+
+df$specie <-  df$Sample |>
+  startsWith('T') |>
+  ifelse('T. vulgaris', 'S. montana')
+
 df |>
   ggplot(aes(
     x = `Properly paired`, y = `error rate`,
-    color = Hyb, shape = Sample)
+    color = Hyb, shape = specie)
     )+
+  # annotate each point with the sample name
+  geom_text(aes(label = Sample), size = 2, vjust = 1.5, hjust = 1.5)+
   geom_point(size = 4)+
   xlab("Percentage of properly paired mapped reads")+
   ylab("N. of mismatches / N. of mapped nucleotides")+
   scale_color_discrete(name = "Hybridization condition")+
-  guides(shape=guide_legend(ncol=2, fill=guide_legend(ncol=2)))+
+  guides(shape=guide_legend(ncol=1, fill=guide_legend(ncol=1)))+
   theme(legend.position="bottom")-> p1
 
 ggsave(
